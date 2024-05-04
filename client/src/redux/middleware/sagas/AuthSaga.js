e3c3f0986508532f22978ef9d459176ab57a9e8a
abhishek37-dulat <abhishek11906997dulat@gmail.com>
2024-05-04 07:35:42 +0530

    one step away from deploy'

--- client/src/redux/middleware/sagas/AuthSaga.js
+++ client/src/redux/middleware/sagas/AuthSaga.js
@@ -9,6 +9,7 @@ export function* register({ payload }) {
     const user = yield call(Register, payload);
     yield put(actions.updateAuthSuccessStatus(true));
     if (user?.data?.status === 200) {
+      console.log(user);
       yield put(actions.loginResponse({ response: user?.data }));
       localStorage.setItem("access_token", user?.data?.access_token);
       success("User Register Successfully");
@@ -32,7 +33,7 @@ export function* authorize({ payload }) {
     if (user?.status === 200 || user?.status === 201) {
       console.log(user);
       localStorage.setItem("access_token", user?.data?.data);
-      yield put(actions.loginResponse({ response: user?.data }));
+      yield put(actions.loginResponse({ response: user?.data?.data }));
       yield put(actions.updateAuthLoading(false));
       success(user?.message);
     } else if (user?.status === 401 || user?.status === 400) {
